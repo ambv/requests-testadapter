9b74247acead4df17bbbe717681bce3cc65123a6
Luke Sneeringer <luke@sneeringer.com>
2013-07-03 10:19:38 -0600

    Eschew super altogether.
    On Python 2, BytesIO is an old-style class.

--- src/requests_testadapter.py
+++ src/requests_testadapter.py
@@ -58,7 +58,7 @@ class Resp(BytesIO):
         return self
 
     def read(self, chunk_size, **kwargs):
-        return super(Resp, self).read(chunk_size)
+        return BytesIO.read(self, chunk_size)
 
     def info(self):
         return self
